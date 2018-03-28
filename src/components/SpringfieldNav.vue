907f44f5c5dbfd7151688a90617ba4ff6bcfff69
Steve Hillier <stephenhillier@gmail.com>
2018-03-27 21:18:59 -0700

    add navbar

---
+++ src/components/SpringfieldNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="primary">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#" style="font-family: 'Rock Salt', cursive;">SPRINGFIELD</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-text>Your neighborhood community centre</b-nav-text>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <!-- <b-navbar-nav class="ml-auto">
+        <b-nav-item>Find activities</b-nav-item>
+      </b-navbar-nav> -->
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'SpringfieldNav'
+
+}
+</script>
+
+<style>
+
+</style>
